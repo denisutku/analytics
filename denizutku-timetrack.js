f3f483aa07fa93b5a78524596939888da2f92723
denisutku <denisutku@gmail.com>
2014-03-21 15:04:12 +0200

    Create denizutku-timetrack.js
    Add this js at the end of body tag to track time on page by triggering an event every 30 seconds.

---
+++ denizutku-timetrack.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+	(function (tos) {
+	  window.setInterval(function () {
+	    tos = (function (t) {
+	      return t[0] == 30 ? (parseInt(t[1]) + 1) + ':00' : (t[1] || '0') + ':' + (parseInt(t[0]) + 30);
+	    })(tos.split(':').reverse());
+ 	    window.pageTracker ? pageTracker._trackEvent('ZiyaretSuresi', 'Dakika', tos) : _gaq.push(['_trackEvent', 'ZiyaretSuresi', 'Dakika', tos]); 
+	  }, 30000);
+	})('00');
+// Bir saniye 1000 milisaniye
+</script>
